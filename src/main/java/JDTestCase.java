019279e3a762c22cf688e29818503d65de1d9441
rajendra8 <rajendra8.sahu@gmail.com>
2022-09-19 19:41:20 +0530

    add into mobileframeowrk2 branch

--- src/main/java/JDTestCase.java
+++ src/main/java/JDTestCase.java
@@ -3,6 +3,8 @@ import org.openqa.selenium.WebElement;
 
 public class JDTestCase {
 
-
+    public static void main(String[] args) {
+        System.out.println("Here i am testing new git branch creation  and adding master branch data into it");
+    }
 
 }
